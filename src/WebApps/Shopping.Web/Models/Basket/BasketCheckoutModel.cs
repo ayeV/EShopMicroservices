1d1f61d6390a07123bd53c499bd77fd9f5380e0d
Ayelen Valdez <ayelenvaldez07@hotmail.com>
2025-02-07 10:16:42 -0300

    Add web client app

---
+++ src/WebApps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class BasketCheckoutModel
+    {
+        public string UserName { get; set; } = default!;
+        public Guid CustomerId { get; set; } = default!;
+        public decimal TotalPrice { get; set; } = default!;
+
+        //Shipping and billing address
+        public string FirstName { get; } = default!;
+        public string LastName { get; } = default!;
+        public string EmailAddress { get; } = default!;
+        public string AddressLine { get; } = default!;
+        public string Country { get; } = default!;
+        public string State { get; } = default!;
+        public string ZipCode { get; } = default!;
+
+        //Payment
+        public string? CardName { get; set; } = default!;
+        public string CardNumber { get; set; } = default!;
+        public string Expiration { get; set; } = default!;
+        public string CVV { get; set; } = default!;
+        public int PaymentMethod { get; set; } = default!;
+    }
+
+    public record CheckoutBasketRequest(BasketCheckoutModel BasketCheckoutDto);
+
+    public record CheckoutBasketResponse(bool IsSuccess);
+
+}
